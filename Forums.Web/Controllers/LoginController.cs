3bb7b7237713b54c6c1ee2df820ec1540661ebca
Andrei <jeman.anrei1@gmail.com>
2024-03-20 18:50:31 +0200

    Login Controller

---
+++ Forums.Web/Controllers/LoginController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.UI.WebControls;
+using System.Web.Optimization;
+
+namespace Forums.Web.Controllers
+{
+    public class LoginController : Controller
+    {
+     
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
