7fd1504e5cd061495746a5ee3171f3fa375bffeb
Jeman Andrei <jeman.andrei1@gmail.com>
2024-06-03 18:25:59 +0300

    Added DbModel - Session and User Context; SessionBL.

---
+++ Forums.BusinessLogic/DBModel/UserContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using Forums.Domain.Entities.User;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace Forums.BusinessLogic.DBModel
+{
+    public class UserContext : DbContext
+    {
+        public UserContext() :
+            base("name=Forums") // ConnectionString name define in FILE Web.config
+        {
+
+        }
+        public virtual DbSet<UDbTable> Users { get; set; }
+
+    }
+}
---
+++ Forums.BusinessLogic/SessionBL.cs
@@ -0,0 +1,45 @@
+﻿using Forums.BusinessLogic.Core;
+using Forums.BusinessLogic.Interfaces;
+using Forums.Domain.Entities.Response;
+using Forums.Domain.Entities.User;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace Forums.BusinessLogic
+{
+    public class SessionBL : UserApi, ISession
+    {
+        public GeneralResp UserPassCheckAction(ULoginData data)
+        {
+            return UserAuthLogic(data);
+        }
+        public GeneralResp RegisterNewUserAction(URegisterData data)
+        {
+            return RegisterUserAction(data);
+        }
+        public HttpCookie GenCookie(string loginCredential)
+        {
+            return Cookie(loginCredential);
+        }
+        public UserMinimal GetUserByCookie(string apiCookieValue)
+        {
+            return UserCookie(apiCookieValue);
+        }
+        public GeneralResp SendEmailToUserAction(string email, string name, string subject, string body)
+        {
+            return SendEmail(email, name, subject, body);
+        }
+        public GeneralResp ResetPasswordAction(string email, string password)
+        {
+            return ResetPassword(email, password);
+        }
+        public GeneralResp ExistingEmailInDB(string email)
+        {
+            return ExistingEmail(email);
+        }
+    }
+}
