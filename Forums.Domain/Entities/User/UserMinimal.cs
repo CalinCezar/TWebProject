258b7646749466db9bfc3022343188ff231ca08d
Cezar Calin <calincezar2@gmail.com>
2024-06-03 18:15:20 +0300

    Domain layer, Entities,Enum

---
+++ Forums.Domain/Entities/User/UserMinimal.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Forums.Domain.Enum;
+
+namespace Forums.Domain.Entities.User
+{
+    public class UserMinimal
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public DateTime LastLogin { get; set; }
+        public string LasIp { get; set; }
+        public UserRole Level { get; set; }
+        public string Photo { get; set; }
+        public string InfoBlog { get; set; }
+        public string Profession { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Fullname { get; set; }
+    }
+}
